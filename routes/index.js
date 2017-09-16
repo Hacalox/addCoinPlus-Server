d4ef54aa0ec21773b5f9349c0b9383c1840d8362
hacalox <hanadonc@gmail.com>
2017-09-16 09:03:18 -0400

    Add resultSending

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
 var request = require('request');
+let finalResult
 
 module.exports = function(app, db) {
   app.get('/number', (req, res) => {
     console.log(req.query.name);
     request('https://api.coin-hive.com/user/balance?secret='+ process.env.KEY_PRIVATE +'&name='+ req.query.name, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
+        finalResult = body
       }
       else{
         console.log(error);
       }
     })
     // You'll create your note here.
-    console.log(req.body);
-    // console.log(req);
-    res.send(req.body)
+    res.send(finalResult)
   });
 };
