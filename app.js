e5ff2f447fc09d4873891ecd6c78fff158bd636d
hacalox <hanadonc@gmail.com>
2017-09-16 19:14:14 -0400

    Add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+var cors = require('cors')
 require('dotenv').config()
 
+app.use(cors())
 const port = process.env.PORT || 8000
 app.use(bodyParser.urlencoded({ extended: true }));
 
