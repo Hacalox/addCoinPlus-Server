800a8ece6212066bf353a9a4d0352b4cee9e00c0
hacalox <hanadonc@gmail.com>
2017-09-16 14:35:46 -0400

    Add dollar capabilities to API

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,41 @@
 var request = require('request');
-let finalResult
 
 module.exports = function(app, db) {
   app.get('/number', (req, res) => {
-    console.log(req.query.name);
     request('https://api.coin-hive.com/user/balance?secret='+ process.env.KEY_PRIVATE +'&name='+ req.query.name, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        finalResult = body
+      let BodyParsed = JSON.parse(body)
+      let hashesMined = BodyParsed.balance
+        request('https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=XMR&tsym=USD', function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            BodyParsed = JSON.parse(body)
+            let netHashesSecond = BodyParsed.Data.NetHashesPerSecond
+            let blockReward = BodyParsed.Data.BlockReward
+            request('https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=USD', function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                BodyParsed = JSON.parse(body)
+                let price = BodyParsed.USD
+                price = parseFloat(price)
+                netHashesSecond = parseFloat(netHashesSecond)
+                blockReward = parseFloat(blockReward)
+                hashesMined = parseFloat(hashesMined)
+                let hashesSecondBlockDay = netHashesSecond / (720 * blockReward)
+                let moneyMade = hashesMined * price / (hashesSecondBlockDay * 24 * 3600)
+                res.send('TotalHashes:'+hashesMined + ',TotalMoney:'+ moneyMade)
+              }
+              else{
+                console.log(error);
+              }
+            })
+          }
+          else{
+            console.log(error);
+          }
+        })
       }
       else{
         console.log(error);
       }
     })
-    // You'll create your note here.
-    res.send(finalResult)
   });
 };
